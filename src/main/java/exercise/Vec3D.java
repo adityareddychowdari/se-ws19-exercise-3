fa908f27257c20ce60829f9eccf7ff4b89e2ee13
Everett <mondliwethu.everet.mthunzi@uni-weimar.de>
2019-11-07 10:43:22 +0100

    implementing the Vec3D methods: double len() Vec3D normalize() Vec3D cross(Vec3D other)

--- src/main/java/exercise/Vec3D.java
+++ src/main/java/exercise/Vec3D.java
@@ -1,14 +1,18 @@
 package exercise;
 
+/**
+ * Vec3D represents a 3-dimensional vector and
+ * supports fundamental mathematical operations.
+ */
 public class Vec3D {
 
-    public float x;
-    public float y;
-    public float z;
+    public double x;
+    public double y;
+    public double z;
 
-    Vec3D(float x, float y, float z ) {
+    public Vec3D(double x, double y, double z) {
         this.x = x;
-        this.y =y;
+        this.y = y;
         this.z = z;
     }
 
@@ -23,21 +27,86 @@ public class Vec3D {
         return "[ " + x + " " + y + " " + z + " ]";
     }
 
-    Vec3D add(Vec3D other) {
+
+    /**
+     * Add operation for 3D vectors
+     *
+     * @param other 3D vector of type Vec3D
+     * @return sum vector of type Vec3D
+     */
+    public Vec3D add(Vec3D other) {
         return new Vec3D(x + other.x, y + other.y, z + other.z);
     }
 
-    Vec3D sub(Vec3D other) {
+
+    /**
+     * subtraction operation for 3D vectors
+     *
+     * @param other 3D vector of type Vec3D
+     * @return difference vector of type Vec3D
+     */
+    public Vec3D sub(Vec3D other) {
         return new Vec3D(x - other.x, y - other.y, z - other.z);
     }
 
-    Vec3D mul(float other) {
+
+    /**
+     * multiplication (scalar) operation for 3D vectors
+     *
+     * @param other 3D vector of type Vec3D
+     * @return resultant vector of type Vec3D
+     */
+    public Vec3D mul(double other) {
         return new Vec3D(x * other, y * other, z * other);
     }
 
-    float mul(Vec3D other) {
+
+    /**
+     * dot product operation for 3D vectors
+     *
+     * @param other 3D vector of type Vec3D
+     * @return resultant double
+     */
+    public double mul(Vec3D other) {
         return x * other.x + y * other.y + z * other.z;
     }
 
-    // TODO: Implement len, cross and norm methods
+
+    /**
+     * magnitude evaluation of a 3D vector (L2 norm)
+     *
+     * @return magnitude of type double
+     */
+    public double len() {
+        return Math.sqrt((x * x) + (y * y) + (z * z));
+    }
+
+
+    /**
+     * cross product of two 3D vectors:
+     *
+     * n.b. The cross product of two 3D vectors is the
+     *      determinant of the corresponding 3 x 3
+     *      [x y z] matrix.
+     *
+     * @param other 3D vector of type Vec3D
+     * @return resultant vector of type Vec3D
+     */
+    public Vec3D cross(Vec3D other) {
+        double xDeterminant = (y * other.z) - (z * other.y);
+        double yDeterminant = - ((x * other.z) - (z * other.x));
+        double zDeterminant = (x * other.y) - (y * other.x);
+        return new Vec3D(xDeterminant, yDeterminant, zDeterminant);
+    }
+
+
+    /**
+     * Normalizing a vector
+     *
+     * @return normalized vector
+     */
+    public Vec3D normalize() {
+        return this.mul(1.0f / this.len());
+    }
 }
+
