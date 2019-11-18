8b6a5ad35297368357f4d6aa37f1dde0f77b960b
Everett <workstation@Everetts-Mac-mini.local>
2019-11-18 14:57:52 +0100

    implementing intersect method based on the point-normal form equation

--- src/main/java/exercise/Plane.java
+++ src/main/java/exercise/Plane.java
@@ -1,124 +1,40 @@
 package exercise;
 
 public class Plane extends Geometry{
+    private Vec3D planePoint, normal;
 
-    public Vec3D origin, normal;
+    public Plane(Vec3D planePoint, Vec3D normal){
+        /* The constructor presupposes using the point-normal form
+         * which employs any initial point on the plane and the plane
+         * normal.
+         */
 
-
-    public Plane(Vec3D origin, Vec3D normal){
-        this.origin = origin;
+        this.planePoint = planePoint;
         this.normal = normal;
     }
 
 
-    private Vec3D noIntersection(){
-        System.out.print("returning null");
-        return null;
-    }
-
-
-    private Vec3D containedIntersection(){
-        System.out.print("returning the point closest to the origin");
-        return null;
-    }
-
-
-    private Vec3D pointIntersection(Vec3D lineVec, Vec3D lp, Vec3D planeNormal, Vec3D pp){
-        System.out.print("returning a single unique point");
-
-        //todo: a convention for naming the points (readable code vs cluttered code)
-            Vec3D diff = lp.sub(pp);
-            double prod1 = diff.dot(planeNormal);
-            double prod2 = lineVec.dot(planeNormal);
-            double prod3 = prod1 / prod2;
-            return lp.sub(lineVec.mul(prod3));
-    }
-
-
     /**
-     * uniqueSolution provides an indication of whether or not a
-     * unique solution exists.
-     *
-     *  @return a true or false indication
-     */
-    private boolean uniqueSolution(){
-
-        /* Possible plane-line intersection points lie within a triangle formed by three
-         * points on the plane; pp0, pp1, and pp2 (where a plane point is denoted by pp).
-         *
-         * --
-         * A line (ray) is described by points between a line origin (lo)
-         * and a line point (denoted lp).
-         *
-         * --
-         * Evaluating the determinant of the intersection:
-         * If the determinant is zero, then there is no unique solution
-         * i.e., the line is either in the plane or parallel to the plane.
-         *
-         *   (-1 x (lineVec)) • ((planeVec01) x (planeVec02))
-         *
-         * <ref: https://en.wikipedia.org/wiki/Line–plane_intersection>
-         */
-
-        Vec3D pp0, pp1, pp2, lo, lp;
-        Vec3D lineVec, planeVec01, planeVec02;
-
-        lo = new Vec3D(1, 1, 1);
-        lp = new Vec3D(1, 1, 1);
-        lineVec = lp.sub(lo);
-
-        pp0 = new Vec3D(1, 1, 1);
-        pp1 = new Vec3D(1, 1, 1);
-        pp2 = new Vec3D(1, 1, 1);
-        planeVec01 = pp1.sub(pp0);
-        planeVec02 = pp2.sub(pp0);
-
-
-        if((lineVec.mul(-1)).dot(planeVec01.cross(planeVec02)) == 0){
-            return false;
-        }
-        else
-            return true;
-    }
-
-    /**
-     * intersect checks for a unique solution relating to a plane-line intersection.
-     * If a unique intersection solution exists, the intersection point closest to
-     * the origin is returned
+     * intersect checks if a provided ray point exists on a given plane,
+     * i.e., if a ray-plane intersection exists. If an intersection exists
+     * the intersection point closest to the origin is returned.
      *
      * @return Vec3D point closest to the origin
      */
     @Override
-    public Vec3D intersect(Vec3D other) {
-        Vec3D point;
+    public Vec3D intersect(Vec3D rayPoint) {
+        /* The point-normal form equation is used to evaluate if the
+         * the ray intersects the plane, i.e., if the ray point lies
+         * on the plane and is orthogonal to the normal
+         *
+         *        normal • ( planePoint - rayPoint ) = 0
+         *
+         * ref: <http://thejuniverse.org/PUBLIC/LinearAlgebra/LOLA/planes/pn.html>
+         */
 
-        if(!uniqueSolution()){
-            // ... check if there is no intersection
-            point = noIntersection();
-
-            // ... only if there is a possible intersection
-            point = containedIntersection();
+        if (normal.dot(rayPoint.sub(planePoint)) == 0){
+            return rayPoint;
         }
-        else
-            //point = pointIntersection();
-        point = null;
-        return point;
+        return null;
     }
-    
-    /* Roger
-	public Vec3D intersect(Vec3D ray) {
-		double t1 = ray.mul(this.normal);
-		Vec3D u = this.origin.sub(ray);
-		double t2 = u.mul(this.normal);
-		
-		// No intersection
-		if (t1 == 0.0) return null;
-
-		// Vector is on the plane
-		if (t1 == 0 && t2 ==0) return ray;
-
-		//Vector intercepts one point on the plane
-		else return ray.mul(t1/t2);
-	}
-		*/
 }
