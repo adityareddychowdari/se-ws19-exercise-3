0777f8be59e5acfb30afa6044619766620d76e99
Nicolai Ruckel <nicolai@ruckel.de>
2019-11-18 15:23:41 +0100

    Add starter code for X3dFile class

---
+++ src/main/java/exercise/X3dFile.java
@@ -0,0 +1,9 @@
+package exercise;
+
+public class X3dFile {
+    public Transform SceneEntities;
+
+    public X3dFile(String filePath) {
+        throw new UnsupportedOperationException("X3dFile constructor not implemented.");
+    }
+}
