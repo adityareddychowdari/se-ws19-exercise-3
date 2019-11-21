0701ab6f00ea1b333acc96c4b098af491fdb920b
everettmthunzi <everettmthunzi@uni-weimar.de>
2019-11-21 12:06:32 +0100

    improving comment clarity

--- src/main/java/exercise/Plane.java
+++ src/main/java/exercise/Plane.java
@@ -4,39 +4,38 @@ public class Plane extends Geometry{
     public Vec3D planePoint, normal;
 
     public Plane(Vec3D planePoint, Vec3D normal){
-        /* The constructor presupposes using the point-normal form
-         * which employs any initial point on the plane and the plane
-         * normal.
-         */
         this.planePoint = planePoint;
         this.normal = normal;
     }
 
 
-    /*
-     * Return intersection of line and plane.
+    /**
+     * This method evaluates an intersection point between an infinite plane and a ray.
+     * Within the same coordinate space, the plane is described using the algebraic form
+     * and the ray originates from the coordinate point [0, 0, 0], i.e., the origin.
+     * If multiple intersection points exist, the intersection point closest to the
+     * origin is returned. If no intersection exists, a null is returned.
      *
-     * Implementation is based on https://en.wikipedia.org/wiki/Line%E2%80%93plane_intersection.
-     * Since the ray always starts in the origin, we can use ray for both l and l_0 (we use the same
-     * Vec3D datatype for points and vectors).
-     * @param ray Line that starts at origin.
-     * @return Intersection point of line and plane. If multiple intersection points exist, the the
-     * closest to the origin is returned. If no intersection exists, return null.
+     * implementation base on:
+     * https://en.wikipedia.org/wiki/Line%E2%80%93plane_intersection.
+     *
+     * @param ray This is a vector describing the rays' line of path (direction) from the origin.
+     * @return Whats returned is the intersection point between the ray and the infinite plane closest to the origin.
      */
     @Override
     public Vec3D intersect(Vec3D ray) {
-        // Check if line and plane are parallel.
+        // check if the ray is contained in the plane (case 1), or if there is no intersection (case 2)
         if (ray.dot(this.normal) == 0.0) {
             if (this.planePoint.sub(ray).dot(this.normal) == 0) {
-                // Line is on the plane. Since the ray starts in the origin, we can always return
-                // that.
+                // If the ray lies on the plane (i.e., is contained by the plane)
+                // the plane also intersects the ray at the origin and the closest
+                // intersection point is thus the origin itself.
                 return new Vec3D(0, 0, 0);
             } else {
-                // The line is parallel to the plane, i.e. there is no intersection.
-                return null;
+                return null; // if there is no intersection a null is returned
             }
         } else {
-            // Compute the intersection point.
+            // compute the single point of intersection (case 3)
             double d = (this.planePoint.sub(ray)).dot(this.normal) / ray.dot(this.normal);
             return ray.mul(d).add(ray);
         }
