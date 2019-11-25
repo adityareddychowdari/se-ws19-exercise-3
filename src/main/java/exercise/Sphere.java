d3fd9182dbd572198f5383a6d22753c29d326d53
Nicolai Ruckel <nicolai@ruckel.de>
2019-11-25 14:55:50 +0100

    Add intersect to Sphere

--- src/main/java/exercise/Sphere.java
+++ src/main/java/exercise/Sphere.java
@@ -12,6 +12,14 @@ public class Sphere extends Geometry{
 
     @Override
     public Vec3D intersect(Vec3D ray) {
-        return null;
+        ray = ray.normalize();
+        double d1 = this.center.dot(ray);
+        double cl = this.center.len();
+        double d2 = d1 * d1 - cl * cl + this.radius * this.radius;
+        if (d2 < 0.0f) {
+            return null;
+        }
+        double d = d1 -  Math.sqrt(d2);
+        return ray.mul(d);
     }
 }
