22a7c7dc7f47cb1e45cd0ffae2988a905c3110a7
Nicolai Ruckel <nicolai@ruckel.de>
2019-11-26 10:20:20 +0100

    Add test cases for cone class

--- src/test/java/exercise/ConeTest.java
+++ src/test/java/exercise/ConeTest.java
@@ -2,12 +2,35 @@ package exercise;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ConeTest {
 
+    final static double DELTA = 0.00001;
+
     @Test
-    public void ConeTest() {
-        // Cone cone = new Cone(1.0, 2.0);
-        // assertEquals(cone.radius, 1.0);
-        // assertEquals(cone.height, 2.0);
+    public void ConeConstructorTest() {
+        Cone cone = new Cone(new Vec3D(1, 1, 0), 1.0, 2.0);
+        assertEquals(cone.apex, new Vec3D(1, 1, 0));
+        assertEquals(cone.radius, 1.0, DELTA);
+        assertEquals(cone.height, 2.0, DELTA);
+    }
+
+    @Test
+    public void ConeIntersectionTest() {
+        Cone cone = new Cone(new Vec3D(2.0, 2.0, 0.0), 2.0, 2.0);
+        Vec3D ray = new Vec3D(2.0, 2.0, 0.0);
+        Vec3D intersection = cone.intersect(ray);
+        assertEquals(intersection.x, 2.0, DELTA);
+        assertEquals(intersection.y, 2.0, DELTA);
+        assertEquals(intersection.z, 0.0, DELTA);
+    }
+
+    @Test
+    public void ConeNoIntersectionTest() {
+        Cone cone = new Cone(new Vec3D(2.0, 2.0, 0.0), 2.0, 2.0);
+        Vec3D ray = new Vec3D(2.0, 0.0, 0.0);
+        Vec3D intersection = cone.intersect(ray);
+        assertEquals(intersection, null);
     }
 }
