f3a0f0fbeae427cb4b79481540d16e4a48d1c91b
Nicolai Ruckel <nicolai@ruckel.de>
2019-11-26 10:02:55 +0100

    Fix line-sphere intersection

--- src/main/java/exercise/Sphere.java
+++ src/main/java/exercise/Sphere.java
@@ -10,19 +10,61 @@ public class Sphere extends Geometry {
         this.center = center;
     }
 
+    /*
+     * Return the intersection point of line and sphere.
+     *
+     * Original code by Thảo Nguyễn.
+     *
+     * @param Line starting at the origin (i.e. (0, 0, 0)).
+     * @return Intersection point closest to the origin. If there exists no intersection, return
+     * null.
+     */
     @Override
     public Vec3D intersect(Vec3D ray) {
-        ray = ray.normalize();
+        // Equation from Wikipedia:
+        // d = -(l·(o-c)) ± √((l·(o-c))ˆ2 - ||o - c||ˆ2 + rˆ2)
+        double d;
 
-        double d1 = this.center.dot(ray);
-        double cl = this.center.len();
-        double d2 = (d1 * d1) - (cl * cl) + (this.radius * this.radius);
+        // Define the origin.
+        Vec3D o = new Vec3D(0, 0, 0);
 
-        if (d2 < 0.0f) {
+        // Calculate the difference between the origin and the sphere's center point.
+        Vec3D diff_o_c = o.sub(this.center);
+
+        // Calculate the dot product of the ray vector and the difference (o - c).
+        double dot_l_oc = ray.dot(diff_o_c);
+
+        // Calculate the distance between the origin and the sphere's center point.
+        double dist_o_c = diff_o_c.len();
+
+        // Calculate the value under the square root, namely delta.
+        double delta = Math.pow(dot_l_oc, 2) - Math.pow(dist_o_c, 2) + Math.pow(this.radius, 2);
+
+        // Due to rounding error, delta only gets close to 0, when it should be 0.
+        // Therefore the comparison "if (delta==0)" can be re-written as "if (|delta|<epsilon)"
+        // where epsilon is an arbitrarily small positive quantity.
+        double epsilon = 0.00001f;
+
+        // The point(s) of intersection can be calculated using the following equation:
+        // x = o + dl
+
+        // If delta = 0, then the line touches the sphere only at one point (one solution).
+        if (Math.abs(delta) < epsilon) {
+            d = - dot_l_oc;
+            return ray.mul(d);
+        }
+
+        // If delta < 0, then the line and the sphere do not intersect (no solution).
+        else if (delta < 0) {
             return null;
         }
 
-        double d = d1 - Math.sqrt(d2);
+        // If delta > 0, then the line intersects the sphere at two points (two solutions).
+        double d1 = -dot_l_oc + Math.sqrt(delta);
+        double d2 = -dot_l_oc - Math.sqrt(delta);
+
+        // Determine the smaller distance and return the point that is closer to the origin.
+        d = Math.min(Math.abs(d1), Math.abs(d2));
         return ray.mul(d);
     }
 }
