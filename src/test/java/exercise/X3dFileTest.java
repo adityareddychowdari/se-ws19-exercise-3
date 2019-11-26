b19198179e17e2bdda295edbfad59803a83fe938
Nicolai Ruckel <nicolai@ruckel.de>
2019-11-26 10:37:23 +0100

    Remove implementation assumptions from X3D tests

--- src/test/java/exercise/X3dFileTest.java
+++ src/test/java/exercise/X3dFileTest.java
@@ -8,9 +8,9 @@ import static org.junit.Assert.assertTrue;
 public class X3dFileTest {
     final static String RESOURCE_PATH = "src/test/resources/";
 
-    /* Test if the constructor loads the X3D file correctly. */
+    /* Test if the constructor loads the X3D file without throwing and exception. */
     @Test
-    public void X3dFileConstructorTest() {
+    public void X3dFileConstructorNoExceptionTest() {
         X3dFile x3d = new X3dFile(RESOURCE_PATH + "test4.x3d");
     }
 }
