8d964e4687fd908e6984390261ce1f8473743c5c
adityareddychowdari <38788147+adityareddychowdari@users.noreply.github.com>
2019-12-04 20:36:47 +0000

    Update Vec3D to take in String parameter

--- src/main/java/exercise/Vec3D.java
+++ src/main/java/exercise/Vec3D.java
@@ -16,6 +16,14 @@ public class Vec3D {
         this.z = z;
     }
 
+    //String handling
+    public Vec3D(String parameter) {
+        String[] data = parameter.split("\\s+");
+        this.x = Double.parseDouble(data[0]);
+        this.y = Double.parseDouble(data[1]);
+        this.z = Double.parseDouble(data[2]);
+    }
+
     public String toString() {
         return "[ " + x + ", " + y + ", " + z + " ]";
     }
